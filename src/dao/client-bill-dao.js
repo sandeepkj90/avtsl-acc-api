7fec145110117c13f8149f3d0b51d0b09eba5882
sandeepkj90 <sandeepkj90@gmail.com>
2024-06-28 16:17:25 +0530

    chnages for Employee Design

--- src/dao/client-bill-dao.js
+++ src/dao/client-bill-dao.js
@@ -1,44 +1,44 @@
 const ClientBillModel = require("../model/client-bill-model");
 const Utility = require("../utils/utility");
 const ClientBillDAO = {
-  addBill: (payload) => {
-    console.log("data inside dao", payload);
-    let date = payload.date ? new Date(payload.date) : new Date();
-    let getDateCode = `${
-      date.getMonth() + 1 < 10
-        ? "0" + (date.getMonth() + 1)
-        : date.getMonth() + 1
-    }-${date.getDate() < 10 ? "0" + date.getDate() : date.getDate()}`;
-    return new ClientBillModel({
-      ...payload,
-      billId: `BILL-${payload.userName.toUpperCase()}-${Utility.generateUID()}-AVTSL-${getDateCode}`,
-    }).save();
-  },
-  isClientExist: (payload) => {
-    return ClientBillModel.findOne({
-      email: payload.email,
-      phone: payload.phone,
-    });
-  },
-  getBillByUserName: (payload) => {
-    let obj = payload || {};
-    // obj.active = true;
-    return ClientBillModel.find(obj, { _id: 0 }).populate({
-      path: "clientId",
-      select: { _id: 0 },
-    });
-  },
-  billPaid: (payload) => {
-    return ClientBillModel.updateOne(
-      { billId: payload.billId },
-      { $set: { status: "PAID" } }
-    );
-  },
-  deleteData: (params, body) => {
-    return ClientBillModel.updateOne({ billId: params.billId }, { $set: body });
-  },
-  updateData: (params, body) => {
-    return ClientBillModel.updateOne({ billId: params.billId }, { $set: body });
-  },
+	addBill: (payload) => {
+		console.log("data inside dao", payload);
+		let date = payload.date ? new Date(payload.date) : new Date();
+		let getDateCode = `${
+			date.getMonth() + 1 < 10
+				? "0" + (date.getMonth() + 1)
+				: date.getMonth() + 1
+		}-${date.getDate() < 10 ? "0" + date.getDate() : date.getDate()}`;
+		return new ClientBillModel({
+			...payload,
+			billId: `BILL-${payload.userName.toUpperCase()}-${Utility.generateUID()}-AVTSL-${getDateCode}`,
+		}).save();
+	},
+	isClientExist: (payload) => {
+		return ClientBillModel.findOne({
+			email: payload.email,
+			phone: payload.phone,
+		});
+	},
+	getBillByUserName: (payload) => {
+		let obj = payload || {};
+		// obj.active = true;
+		return ClientBillModel.find(obj, { _id: 0 }).populate({
+			path: "clientId",
+			select: { _id: 0 },
+		});
+	},
+	billPaid: (payload) => {
+		return ClientBillModel.updateOne(
+			{ billId: payload.billId },
+			{ $set: { status: "PAID" } },
+		);
+	},
+	deleteData: (params, body) => {
+		return ClientBillModel.updateOne({ billId: params.billId }, { $set: body });
+	},
+	updateData: (params, body) => {
+		return ClientBillModel.updateOne({ billId: params.billId }, { $set: body });
+	},
 };
 module.exports = ClientBillDAO;
--- src/dao/user-dao.js
+++ src/dao/user-dao.js
@@ -1,76 +1,80 @@
 const UserModel = require("../model/user-model");
 const Utility = require("../utils/utility");
 const UserDAO = {
-  register: async (payload) => {
-    console.log("data inside dao", payload);
-    let result = (
-      await UserModel.aggregate([
-        { $match: {} },
-        { $project: { _id: 0, userName: 1 } },
-        { $unwind: "$userName" },
-        { $group: { _id: null, result: { $addToSet: "$userName" } } },
-      ])
-    )[0]?.result;
+	register: async (payload) => {
+		console.log("data inside dao", payload);
+		let result = (
+			await UserModel.aggregate([
+				{ $match: {} },
+				{ $project: { _id: 0, userName: 1 } },
+				{ $unwind: "$userName" },
+				{ $group: { _id: null, result: { $addToSet: "$userName" } } },
+			])
+		)[0]?.result;
 
-    if (result?.length > 0) result = Utility.calcUserId(result);
-    else result = "01";
-    // console.log(result);
-    // return true;
-    return new UserModel({
-      userName: `AV${result}`,
-      ...payload,
-    }).save();
-  },
-  isUserExist: (payload) => {
-    return UserModel.findOne({
-      userName: payload.userName,
-    });
-  },
-  getByCondition: (payload) => {
-    return UserModel.find(payload);
-  },
-  updateOTP: (payload, otp) => {
-    return UserModel.updateOne(
-      { email: payload.email },
-      { $set: { otp: otp } }
-    );
-  },
-  verifyOTP: (payload) => {
-    return UserModel.findOne({ email: payload.email, otp: payload.otp });
-  },
-  updatePassword: (payload) => {
-    return UserModel.updateOne(
-      { email: payload.email },
-      { $set: { password: payload.password } }
-    );
-  },
-  getEmployeeList: (payload) => {
-    let obj = payload || {};
-    if (payload.role) {
-      obj.role = { $ne: payload.role };
-    } else {
-      obj["role"] = "EMPLOYEE";
-    }
-    console.log(obj);
-    return UserModel.find(obj, { password: 0 });
-  },
-  approve: (payload) => {
-    return UserModel.updateOne(
-      { userName: payload.userName },
-      { $set: { status: "APPROVED" } }
-    );
-  },
-  getTechnician: () => {
-    return UserModel.find(
-      { role: "TECHNICIAN", status: "APPROVED" },
-      { _id: 1, firstName: 1, phone: 1, skills: 1 }
-    );
-  },
-  deleteData: (params, body) => {
-    return UserModel.updateOne({ userName: params.userName }, { $set: body });
-  },
-  updateData: (params, body) => {
-    return UserModel.updateOne({ userName: params.userName }, { $set: body });
-  },
+		if (result?.length > 0) result = Utility.calcUserId(result);
+		else result = "01";
+		// console.log(result);
+		// return true;
+		return new UserModel({
+			userName: `AV${result}`,
+			...payload,
+		}).save();
+	},
+	isUserExist: (payload) => {
+		return UserModel.findOne({
+			userName: payload.userName,
+		});
+	},
+	getByCondition: (payload) => {
+		return UserModel.find(payload);
+	},
+	updateOTP: (payload, otp) => {
+		return UserModel.updateOne(
+			{ email: payload.email },
+			{ $set: { otp: otp } },
+		);
+	},
+	verifyOTP: (payload) => {
+		return UserModel.findOne({ email: payload.email, otp: payload.otp });
+	},
+	updatePassword: (payload) => {
+		return UserModel.updateOne(
+			{ email: payload.email },
+			{ $set: { password: payload.password } },
+		);
+	},
+
+	getEmployeeList: (payload) => {
+		let obj = payload || {};
+		if (payload.role) {
+			obj.role = { $ne: payload.role };
+		} else {
+			obj["role"] = "EMPLOYEE";
+		}
+		console.log(obj);
+		if (!obj.active) {
+			obj["active"] = true;
+		}
+		return UserModel.find(obj, { password: 0 }).sort({ date: -1 });
+	},
+	approve: (payload) => {
+		return UserModel.updateOne(
+			{ userName: payload.userName },
+			{ $set: { status: "APPROVED" } },
+		);
+	},
+	getTechnician: () => {
+		return UserModel.find(
+			{ role: "TECHNICIAN", status: "APPROVED" },
+			{ _id: 1, firstName: 1, phone: 1, skills: 1 },
+		);
+	},
+	deleteData: (params, body) => {
+		return UserModel.updateOne({ userName: params.userName }, { $set: body });
+	},
+	updateData: (params, body) => {
+		return UserModel.updateOne({ userName: params.userName }, { $set: body });
+	},
 };
 module.exports = UserDAO;
