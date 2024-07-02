26998968910fb8283caf2032a57a816148272bf7
sandeepkj90 <sandeepkj90@gmail.com>
2024-07-02 13:41:32 +0530

    COmmit for fix

--- start.sh
+++ start.sh
@@ -9,7 +9,9 @@ if git clone https://github.com/sandeepkj90/avtsl-acc-api.git ; then
   echo "Repository cloned successfully"
   cd avtsl-acc-api
   npm install
+  pm2 stop index.js
   pm2 start index.js
+
 else
   echo "Failed to clone repository"
   exit 1
