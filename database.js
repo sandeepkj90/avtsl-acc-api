58f977f578ef5b369a6212aaef593b73f9bf0a0b
sandeepkj90 <sandeepkj90@gmail.com>
2024-08-04 12:06:10 +0400

    Update database.js

--- database.js
+++ database.js
@@ -4,8 +4,8 @@ const Constant = require("./src/utils/constant");
 // let database_url = process.env.MONGODB_URL;
 mongoose
 	.connect(
-		// "mongodb+srv://sandeepkj90:avtsl%40sandy@cluster0.0votf7u.mongodb.net/avtsl-acc?retryWrites=true&w=majority&appName=Cluster0"
-		"mongodb+srv://sivareddyega:siva%40avtsl123@cluster0.hmonxox.mongodb.net/avtsl-acc?retryWrites=true&w=majority&appName=Cluster0",
+		"mongodb+srv://sandeepkj90:avtsl%40sandy@cluster0.0votf7u.mongodb.net/avtsl-acc?retryWrites=true&w=majority&appName=Cluster0"
+		//"mongodb+srv://sivareddyega:siva%40avtsl123@cluster0.hmonxox.mongodb.net/avtsl-acc?retryWrites=true&w=majority&appName=Cluster0",
 	)
 	.then(() => console.log("Connected!"));
 module.exports = mongoose;
